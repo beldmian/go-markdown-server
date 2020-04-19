c6ab35a1adde47f6ee68c386c00a927eeee87d27
beldmian <beldmian@gmail.com>
2020-04-19 21:56:18 +0300

    Uses mongodb

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.13
 
 require (
 	github.com/gorilla/mux v1.7.4
+	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/russross/blackfriday v2.0.0+incompatible
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.3.2
 )
--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/gorilla/mux"
 	"github.com/russross/blackfriday"
@@ -16,52 +21,93 @@ import (
 type CreateRequest struct {
 	Title string `json:"title"`
 	Body  string `json:"body"`
+	URL   string `json:"url"`
+}
+
+var (
+	port       string
+	collection *mongo.Collection
+)
+
+func init() {
+	if os.Getenv("PORT") != "" {
+		port = ":" + os.Getenv("PORT")
+	} else {
+		port = ":8080"
+	}
 }
 
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://beld:124252@cluster0-wmuco.mongodb.net/blog"))
+	collection = client.Database("blog").Collection("posts")
+	if err != nil {
+		log.Fatal(err)
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler)
 	r.HandleFunc("/post/{name}", mdNamedHandler)
 	r.HandleFunc("/add", addHandler)
 	fmt.Println("Server have started")
-	http.ListenAndServe(":"+os.Getenv("PORT"), r)
+	log.Print("Server started", port)
+	if err := http.ListenAndServe(port, r); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	file, _ := ioutil.ReadFile("md/index.md")
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	out := "# Home\n---\n"
+	ctx5, cancel5 := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel5()
+	for cursor.Next(ctx5) {
+		var post CreateRequest
+		err := cursor.Decode(&post)
+		if err != nil {
+			log.Fatal(err)
+		}
+		out += "- [" + post.Title + "](/post/" + post.URL + ")\n"
+	}
 	tmpl := template.Must(template.ParseFiles("md.html"))
-	output := template.HTML(string(blackfriday.Run(file)))
-	tmpl.ExecuteTemplate(w, "md", output)
+	output := template.HTML(string(blackfriday.Run([]byte(out))))
+	if err := tmpl.ExecuteTemplate(w, "md", output); err != nil {
+		log.Fatal(err)
+	}
 }
 func mdNamedHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	file, err := ioutil.ReadFile("md/" + vars["name"] + ".md")
-	if err != nil {
-		fmt.Fprint(w, "File Not found")
-	} else {
-		tmpl := template.Must(template.ParseFiles("md.html"))
-		output := template.HTML(string(blackfriday.Run(file)))
-		tmpl.ExecuteTemplate(w, "md", output)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	filter := bson.M{"url": vars["name"]}
+	var post CreateRequest
+	if err := collection.FindOne(ctx, filter).Decode(&post); err != nil {
+		log.Fatal(err)
 	}
+	tmpl := template.Must(template.ParseFiles("md.html"))
+	output := template.HTML(string(blackfriday.Run([]byte(post.Body))))
+	tmpl.ExecuteTemplate(w, "md", output)
 }
 
 func addHandler(w http.ResponseWriter, r *http.Request) {
+	//if r.FormValue("key") == "124252" {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	req := CreateRequest{
-		Title: r.FormValue("Title"),
-		Body:  r.FormValue("Body"),
+		Title: r.FormValue("title"),
+		Body:  r.FormValue("body"),
+		URL:   r.FormValue("url"),
 	}
-	f, err := os.Create("./md/" + req.Title + ".md")
+	obj, err := collection.InsertOne(ctx, req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	f.Write([]byte(req.Body))
-	defer f.Close()
-	index, err := os.OpenFile("./md/index.md", os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err = index.WriteString("\n- [" + req.Title + "](/post/" + req.Title + ")"); err != nil {
-		log.Fatal(err)
-	}
-	defer index.Close()
+	log.Print("Add object with id", obj.InsertedID)
+	w.Write([]byte("add success"))
+	//}
 }
